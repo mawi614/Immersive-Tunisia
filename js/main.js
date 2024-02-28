3ec90520d3d35d9be10b04c95e021b0e38756c60
Mohamed Amine Elmaaoui <63106502+mawi614@users.noreply.github.com>
2024-02-28 23:43:40 +0100

    Major Update
    - Changed Page Layout (Didn't change canvas position)
    - Added Content
    - Added Language changer
    - Fixed a bunch of stuff

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Static data goes here
-const MonumentNames = [
-'Beb Alioua',
+const MonumentNamesEN = [
+'Beb Dzira',
 'Beb Bhar',
 'Beb Bnet',
 'Beb El Falla',
@@ -11,26 +11,147 @@ const MonumentNames = [
 'Beb Mnara',
 'Beb Saadoun'
 ];
-const MonumentDescriptions = [
-    'LOREM IPSUM 1',
-    'LOREM IPSUM 2',
-    'LOREM IPSUM 3',
-    'LOREM IPSUM 4',
-    'LOREM IPSUM 5',
-    'LOREM IPSUM 6',
-    'LOREM IPSUM 7',
-    'LOREM IPSUM 8',
-    'LOREM IPSUM 9',
-    'LOREM IPSUM 10'
+const MonumentNamesFR = MonumentNamesEN;
+const MonumentNamesAR = [
+    'باب الجزيرة',
+    'باب البحر',
+    'باب بنات',
+    'باب الفلة',
+    'باب الجديد',
+    'باب العلوج',
+    'باب القصبة',
+    'باب الخضراء',
+    'باب المنارة',
+    'باب سعدون'
+];
+
+    
+const MonumentDescriptionsEN = [
+    //Beb Dzira Desc
+    `One of the oldest gates of the ancient city, so named because it opens into the peninsula of Watan al-Qibli and overlooks the island of Shrik al-Absi, who was the governor of Watan al-Qibli (now Nabeul)`,
+    //Beb Dhar Desc
+    `Built during the reign of the Aghlaba, in the eastern wall of the old city. It was so named because it opened towards the sea, facing Lake Tunis, and facilitated movement between the city and the harbor on the lake. In 1860, after the demolition of the walls, the door was dismantled and reinstalled at the entrance to Bahariya Street, at the request of the French Consul Leon Roche, who had received permission from the Bey of Tunis in 1857 to build the consulate outside the walls on Bahariya Street. Bab Bahr subsequently took the name Bab France during the French colonization of Tunisia, a name that is still sometimes used today
+
+    On November 23, 1925, French Resident-General Lucien Saint presented a statue of Cardinal Charles Martial Lavigri holding a cross. The municipality of Tunis decided to place it in the square overlooking Bab Bahr. It was dismantled in 1962 by the decision of Mayor Ali Belhouane. It was moved to the garden of the Carthage Museum
+    `,
+    //Beb Bnet Desc
+    `It was the fifth door to be opened in the city walls between 1228 and 1249 AD. The role of this door was to separate the old city of Tunis from the neighborhood of the Christian relatives of the Hafsid sultans, and it is currently demolished
+
+    It was named Bab al-Banat because Sultan Abu Zakaria al-Hafsi (1223-1249) adopted the three daughters of his enemy Yahya bin Ghanya after he was killed, as the latter sent his daughter before the battle with a letter containing the content of caring for his daughter in the event of his defeat and death and taking them back in the event of his victory, and Abu Zakaria al-Hafsi raised them in a palace located near this door
+    `,
+    //Beb Elfalleh Desc
+    `Bab El Fellah was named Bab El Fellah because it was just a hole or "fella" in the wall of the old city. It was used to flee when the Spaniards occupied the city in the 16th century or the word "fella" or "fel" is a common and widely used word in Tunisia or means escape or departure depending on the context of the sentence.
+
+    It is also called Bab al-Serdab, a door located on the second wall south of the city. Built in 1350, it has an important economic role as it opens onto the roads leading to Kairouan and Zaghouan. There is a crypt next to the door that allowed the city's inhabitants to escape during Charles V's capture of the metropolis in 1535 AD when the Spaniards approached. This door was demolished before 1890.
+    `,
+    //Beb Jdid Desc
+    `Bab al-Jadid is the sixth door that was added to the city walls during the reign of Sultan Yahya al-Hafsi in 1278 AD. This door was demolished during the Pasha Husseiniya sedition in the second quarter of the eighteenth century, and was renovated during the reign of Ali Bey bin Hussein in 1769. It opens onto a street of the same name, also known as Bab al-Haddadah, because the door leads to the blacksmith's market.`,
+    //Beb Laalouj Desc
+    `It was built by the Hafsidi Sultan Abu Ishaq Ibrahim al-Mustansir (1349-1369) and called Bab al-Rahiba (meaning the door to the small square). In 1435 AD, Hafsi Sultan Abu Amr Othman summoned his mother's family from Italy and settled them in the neighborhood next to the Kasbah, which became known as Rahba al-Alouj (from al-Alouj, a European foreigner), and thus the door was called Bab al-Alouj.`,
+    //Beb Kasbah Desc
+    `The Kasbah Gate opens onto the Kasbah and is intended for the passage of rulers, their entourage and ministers, not for the general public.`,
+    //Beb Khadhra Desc
+    `It is located north of the city and was built around 1320 AH in the form of a simple arch. It was demolished and rebuilt in its current form in 1881 to facilitate commercial exchanges. Its name is one of the names of the city of Tunis "Green", but it also bears the name because of the green orchards that extend from the wall to the borders of Ariana and Carthage.`,
+    //Beb Mnara Desc
+    `It was built in 1276 AD on the western walls of the city during the Hafsid era and took the place of "Bab Arta", and Arta is the name of a military leader, who was said to have been killed on the spot where the door was built in the battles for the conquest of Tunis.
+
+    It was also called Bab al-Manara after the lighthouse that was located between Bab al-Jadid and Kasbah Square. 
+    
+    The city of Tunis closes its 6 doors at night, and only the Menara door remains open to receive people coming in late at night, and we find outside it a lantern that illuminates the people coming
+    `,
+    //Beb Saadoun Desc
+    `Built around 1350 next to Bab Souika, its name came from the name of a good man named Sidi Bou Saadoun, who lived in its neighborhood. In the fifteenth century AD, the watering can of this door was built in 838 AH/1434 AD during the reign of the Hafsi Sultan Muhammad al-Muntasser. This door opens to the roads of Beja, Bizerte and El Kef. It originally had a single arch, but since 1881 it has had three arches to facilitate commercial transactions`
+];
+
+const MonumentDescriptionsFR = [
+    //Beb Dzira Desc
+    `L'une des plus anciennes portes de la ville antique, ainsi nommée parce qu'elle s'ouvre sur la péninsule de Watan al-Qibli et surplombe l'île de Shrek al-Absi, qui était le gouverneur de Watan al-Qibli (aujourd'hui Nabeul).`,
+    //Beb Bhar Desc
+    `Construite sous le règne des Aghlaba, dans l'enceinte orientale de la vieille ville. Elle était ainsi nommée parce qu'elle s'ouvrait vers la mer, face au lac de Tunis, et facilitait la circulation entre la ville et le port du lac. En 1860, après la démolition des murailles, la porte fut démontée et réinstallée à l'entrée de la rue Bahariya, à la demande du consul français Léon Roche, qui avait obtenu du bey de Tunis, en 1857, l'autorisation de construire le siège du consulat à l'extérieur des murailles de la rue Bahariya. Bab Bahr prit ensuite le nom de Bab France pendant la colonisation française de la Tunisie, nom qui est encore parfois utilisé aujourd'hui
+
+    Le 23 novembre 1925, le résident général de France à Tunis, Lucien Saint, offre une statue du cardinal Charles Martial Lavigri tenant une croix. La municipalité de Tunis décide de la placer sur la place qui domine Bab Bahr. Elle fut démontée en 1962 sur décision du maire Ali Belhouane. Elle fut déplacée dans le jardin du musée de Carthage
+    `,
+    //Beb Bnet Desc
+    `Il s'agit de la cinquième porte ouverte dans les remparts de la ville entre 1228 et 1249 après J.-C. Cette porte avait pour fonction de séparer la vieille ville de Tunis du quartier des parents chrétiens des sultans hafsides, et elle est actuellement démolie
+
+    Elle fut nommée Bab al-Banat car le sultan Abu Zakaria al-Hafsi (1223-1249) adopta les trois filles de son ennemi Yahya bin Ghanya après qu'il eut été tué, ce dernier ayant envoyé sa fille avant la bataille avec une lettre contenant le message qu'il prendrait sa fille en cas de défaite et de mort et qu'il les reprendrait en cas de victoire, et Abu Zakaria al-Hafsi les éleva dans un palais situé à proximité de cette porte
+    `,
+    //Beb Elfalleh Desc
+    `Bab El Fellah a été nommé ainsi parce qu'il s'agissait d'un trou ou "fella" dans le mur de la vieille ville. Il a été utilisé pour fuir lorsque les Espagnols ont occupé la ville au 16ème siècle. Le mot "fella" ou "fel" est un mot commun et largement utilisé en Tunisie et signifie fuir ou quitter selon le contexte de la phrase.
+
+    Elle est aussi appelée Bab al-Serdab, une porte située sur la deuxième enceinte au sud de la ville. Construite en 1350, elle a un rôle économique important puisqu'elle ouvre sur les routes menant à Kairouan et Zaghouan. A côté de la porte se trouve une crypte qui permettait aux habitants de la ville de s'échapper lors de la prise de la métropole par Charles Quint en 1535 à l'approche des Espagnols. Cette porte a été démolie avant 1890.
+    `,
+    //Beb Jdid Desc
+    `Bab al-Jadid est la sixième porte ajoutée aux murs de la ville sous le règne du sultan Yahya al-Hafsi en 1278 après J.-C. Cette porte a été démolie pendant la sédition du pacha Husseiniya dans le deuxième quart du XVIIIe siècle. Cette porte a été démolie pendant la sédition du Pacha Husseiniya dans le deuxième quart du XVIIIe siècle, et a été rénovée sous le règne d'Ali Bey ibn Hussein en 1769. Elle donne sur une rue du même nom, également connue sous le nom de Bab al-Haddadah, car la porte mène au marché des forgerons.`,
+    //Beb Laalouj Desc
+    `Elle a été construite par le sultan hafside Abou Ishaq Ibrahim al-Mustansir (1349-1369) et appelée Bab al-Rahiba (qui signifie la porte de la petite place). En 1435, le sultan hafside Abou Amr Othman fit venir d'Italie la famille de sa mère et l'installa dans le quartier voisin de la kasbah, connu sous le nom de Rahba al-Alouj (d'al-Alouj, étranger européen), d'où le nom de Bab al-Alouj.`,
+    //Beb Lkasbah Desc
+    `La porte de la Kasbah s'ouvre sur la Kasbah et est destinée au passage des souverains, de leur entourage et de leurs ministres, et non au grand public.`,
+    //Beb Khadhra Desc
+    `Il est situé au nord de la ville et a été construit vers 1320 de l'Hégire sous la forme d'un simple arc. Elle a été démolie et reconstruite sous sa forme actuelle en 1881 pour faciliter les échanges commerciaux. Son nom est l'un des noms de la ville "verte" de Tunis, mais elle porte aussi ce nom en raison des vergers verdoyants qui s'étendent de la muraille jusqu'aux frontières de l'Ariana et de Carthage.`,
+    //Beb Mnara Desc
+    `Elle a été construite en 1276 sur les murs ouest de la ville à l'époque hafside et a pris la place de "Bab Arta". Arta est le nom d'un commandant militaire qui aurait été tué à l'endroit où la porte a été construite lors des batailles pour la conquête de Tunis.
+
+    Elle était aussi appelée Bab al-Manara, du nom du phare qui se trouvait entre Bab al-Jadid et la place de la Kasbah. 
+    
+    La ville de Tunis ferme ses 6 portes la nuit et seule la porte de la Ménara reste ouverte pour accueillir les gens qui entrent la nuit, et on trouve une lanterne à l'extérieur pour éclairer les gens qui entrent
+    `,
+    //Beb Saadoun Desc
+    `Construite vers 1350 à côté de Bab Souika, son nom vient de celui d'un brave homme nommé Sidi Bou Saadoun, qui vivait dans son voisinage. Au XVe siècle de notre ère, l'abreuvoir de cette porte a été construit en 838 de l'Hégire/1434 de l'Hégire sous le règne du sultan hafside Muhammad al-Muntasser. Cette porte s'ouvre sur les routes de Béja, Bizerte et El Kef. Elle avait à l'origine une seule arche, mais depuis 1881 elle a trois arches pour faciliter les transactions commerciales.`
+];
+
+const MonumentDescriptionsAR = [
+    //Beb Dzira Desc
+    `أحد أقدم أبواب المدينة العتيقة، سمي كذلك لأنه ينفذ إلى شبه جزيرة الوطن القبلي ويشرف على جزيرة “شريك العبسي”، الذي كان واليا لمنطقة الوطن القبلي (نابل حاليا)`,
+    //Beb Bhar Desc
+    ` بني في عهد الأغالبة، في السور الشرقي للمدينة العتيقة. سمي كذلك لأنه يفتح في اتجاه البحر، قبالة بحيرة تونس وقد كان يسهل الحركة بين المدينة والمرفأ في البحيرة. في 1860، وبعد هدم الأسوار فكك الباب وأعيد تركيبه في مدخل شارع البحرية، بطلب من قنصل فرنسا ليون روشي، والذي كان قد أخذ من باي تونس في 1857 الإذن ببناء مقر القنصلية خارج الأسوار على شارع البحرية. اثر ذلك اخذ باب بحر اسم باب فرنسا خلال الاستعمار الفرنسي لتونس، وهو اسم ما زال يستعمل أحيانا اليوم
+
+    في 23 نوفمبر 1925، المقيم العام الفرنسي بتونس لوسيان سان اهدى تمثال الكردنال شارل مارسيال لافيجري حاملا الصليب. فقررت بلدية مدينة تونس وضعه في الساحة المطلة على باب بحر. فكك سنة 1962 بقرار من العمدة علي البلهوان. ونقل إلى حديقة متحف قرطاج.
+    `,
+    //Beb Bnet Desc
+    `وهو خامس باب يُفتح في أسوار المدينة بين عامي 1228 و1249 م، و كان دور هذا الباب هو فصل مدينة تونس العتيقة عن حي الأقارب المسيحيين للسلاطين الحفصيين وهو حاليًا مهدّم
+
+    سمي بـباب البنات لأن السلطان أبو زكريا الحفصي (1223-1249) استبنى بنات عدوه يحي بن غانية الثلاث بعد مقتله، حيث أرسل الأخير بنته قبل المعركة مع رسالة مضمونها الإعتينأ ببنته في صورة هزيمته ومقتله وأسترجعهم في حالة فوزه ، ورباهن أبو زكريا الحفصي في قصر واقع قرب هذا الباب
+    `,
+    //Beb Elfalleh Desc
+    `باب الفلة سمي بـباب الفلة نظرا لأنه كان مجرد ثلمة أو "فلة" في سور المدينة العتيقة. وقد استعمل لفرار السكان عندما احتل الإسبان المدينة في القرن السادس عشر أو كلمة "فلة" أو "فل" هي كلمة شائعة و مستعملة بكثرة في تونس أو تعني الهرب أو المغادرة حسب سياق الجملة .
+
+    سمي أيضا باب السرداب، وهو باب يقع على السور الثاني جنوب المدينة. بني سنة 1350، له دور اقتصادي هام فهو يفتح على الطرق المؤدية إلى القيروان وزغوان. هناك بجانب الباب سرداب سمح بهروب ساكني المدينة خلال استيلاء شارل الخامس على الحاضرة في 1535 م عند اقتراب الإسبان. هدم هذا الباب قبل عام 1890
+    `,
+    //Beb Jdid Desc
+    `باب الجديد هو الباب السادس الذي أحدث في أسوار المدينة في عهد السلطان يحي الحفصي سنة 1278 م. هدم هذا الباب خلال الفتنة الباشية الحسينية في الربع الثاني من القرن الثامن عشر، ووقع تجديده في عهد علي باي بن حسين سنة 1769م. يفتح على شارع بنفس الاسم، يعرف أيضا باسم باب الحدادة، لأن الباب ينفذ إلى سوق الحدادة
+    `,
+    //Beb Laalouj Desc
+    `بناه السلطان الحفصي أبو إسحاق إبراهيم المستنصر (1349-1369)، وسماه باب الرحيبة (أي باب الساحة الصغيرة). في عام 1435م  قام السلطان الحفصي أبو عمرو عثمان باستدعاء عائلة أمه من إيطاليا، وأسكنها بالحي المجاور للقصبة، والذي صار يسمى رحبة العلوج (من العلج، وهو الأجنبي الأوروبي)، وبذلك سمي الباب بـباب العلوج.
+    `,
+    //Beb Lkasbah Desc
+    `يفتح على القصبة وهو مخصص لعبور الحكام وحاشيتهم ووزرائهم وليست للعامة.
+    `,
+    //Beb Khadhra Desc
+    `يقع شمال المدينة بني حوالي 1320 هـ على شكل قوس بسيط. هدم وأعيد بناؤه على شكله الحالي في 1881 لتسهيل المبادلات التجارية. اسمه هو أحد تسميات مدينة تونس “الخضراء”، ولكنه يحمل الاسم أيضا بسبب البساتين الخضراء التي تمتد من السور إلى حدود أريانة وقرطاج.
+    `,
+    //Beb Mnara Desc
+    `بني عام 1276م على الأسوار الغربية للمدينة في عهد الحفصيين و أخذ مكان “باب أرطة”، وأرطة هو اسم قائد عسكري، قيل إنه قتل في المكان الذي أنشئ فيه الباب في معارك فتح مدينة تونس.
+
+    سمي بـباب المنارة كذلك نسبة إلى المشكاة التي كانت بجداره لهداية المارة ، و يقع الباب بين باب الجديد و ساحة القصبة 
+    
+    مدينة تونس تغلق ابوابها ال6 ليلا و لا يبقى سوى باب منارة مفتوح لستقبل الناس القادمة في اوخر ليل و نجد في خارجه قنديل ينير للأشخاص الأتية. و نقلوا بالتونسي" يضوي على البراني" أو هل جملة الأخيرة أصبحت مثل شعبي
+    `,
+    //Beb Saadoun Desc
+    `بني حوالي سنة 1350 بجانب باب سويقة، وقد جاء اسمه من اسم رجل صالح يدعى سيدي بو سعدون،كان يعيش في جواره. وفي القرن الخامس عشر ميلادي بنيت سقاية هذا الباب سنة 838 هـ/1434م في عهد السلطان الحفصي محمد المنتصر. يفتح هذا الباب على طرق باجة وبنزرت والكاف. كان في الاصل يحتوي على قوس واحد، وأصبح منذ سنة 1881 بثلاثة أقواس لتسهيل الميادلات التجارية
+    `
 ];
 
 // Gettting refs to the page's elements
 var MonumentName = document.getElementById('monument-name');
 var MonumentText = document.getElementById('monument-description');
-var appframe = document.getElementById("app-frame");
 
 // Keeps track of the current monument
 var currentSet = 0;
+
+// Language Set (EN Default)
+var MonumentNames = MonumentNamesEN;
+var MonumentDescriptions = MonumentDescriptionsEN;
 
 /*--Goes to next monument--*/
 function next(){
@@ -44,7 +165,7 @@ function next(){
     MonumentText.innerText = MonumentDescriptions[currentSet];
 
     // load next scene
-    loadScene("Monument".concat(currentSet),{hierarchy:!0,settings:!1});
+    loadScene("Monument".concat(currentSet+1),{hierarchy:!0,settings:!1});
 
     // Fading in
     MonumentName.animate({opacity:[0,1]},{duration:2000})
@@ -67,8 +188,51 @@ function previous(){
     MonumentText.innerText = MonumentDescriptions[currentSet];
 
     // load previous scene
-    loadScene("Monument".concat(currentSet),{hierarchy:!0,settings:!1});
+    loadScene("Monument".concat(currentSet+1),{hierarchy:!0,settings:!1});
     // Fade in
+    MonumentName.animate({opacity:[0,1]},{duration:2000})
+    MonumentText.animate({opacity:[0,1]},{duration:2000});
+}
+
+/*--Changing the language--*/
+function changeLangToAR(){
+    MonumentName.animate({opacity:[1,0]},{duration:2000});
+    MonumentText.animate({opacity:[1,0]},{duration:2000});
+
+    MonumentNames = MonumentNamesAR;
+    MonumentDescriptions = MonumentDescriptionsAR;
+
+    MonumentName.innerText = MonumentNames[currentSet];
+    MonumentText.innerText = MonumentDescriptions[currentSet];
+
+    MonumentName.animate({opacity:[0,1]},{duration:2000})
+    MonumentText.animate({opacity:[0,1]},{duration:2000});
+}
+
+function changeLangToFR(){
+    MonumentName.animate({opacity:[1,0]},{duration:2000});
+    MonumentText.animate({opacity:[1,0]},{duration:2000});
+
+    MonumentNames = MonumentNamesFR;
+    MonumentDescriptions = MonumentDescriptionsFR;
+
+    MonumentName.innerText = MonumentNames[currentSet];
+    MonumentText.innerText = MonumentDescriptions[currentSet];
+
+    MonumentName.animate({opacity:[0,1]},{duration:2000})
+    MonumentText.animate({opacity:[0,1]},{duration:2000});
+}
+
+function changeLangToEN(){
+    MonumentName.animate({opacity:[1,0]},{duration:2000});
+    MonumentText.animate({opacity:[1,0]},{duration:2000});
+
+    MonumentNames = MonumentNamesEN;
+    MonumentDescriptions = MonumentDescriptionsEN;
+
+    MonumentName.innerText = MonumentNames[currentSet];
+    MonumentText.innerText = MonumentDescriptions[currentSet];
+
     MonumentName.animate({opacity:[0,1]},{duration:2000})
     MonumentText.animate({opacity:[0,1]},{duration:2000});
 }
